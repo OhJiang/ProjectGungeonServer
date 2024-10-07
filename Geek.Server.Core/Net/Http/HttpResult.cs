ddc219885d0d4a665e75fd9a18e305fac412495f
OhJiang <14795402723@163.com>
2024-10-07 17:35:10 +0800

    feat: HttpResult 的 Code 数据类型改为 int

--- Geek.Server.Core/Net/Http/HttpResult.cs
+++ Geek.Server.Core/Net/Http/HttpResult.cs
@@ -4,81 +4,87 @@ using System.Text.Unicode;
 
 namespace Geek.Server.Core.Net.Http
 {
+	public class HttpResult
+	{
+		public enum Stauts
+		{
+			///<summary>成功</summary>
+			Success = 200,
 
+			///<summary>未定义的命令</summary>
+			UnDefine = 11,
 
+			///<summary>非法</summary>
+			Illegal = 12,
 
-    public class HttpResult
-    {
-        public enum Stauts
-        {
-            ///<summary>成功</summary>
-            Success = 200,
-            ///<summary>未定义的命令</summary>
-            Undefine = 11,
-            ///<summary>非法</summary>
-            Illegal = 12,
-            ///<summary>参数错误</summary>
-            ParamErr = 13,
-            ///<summary>验证失败</summary>
-            CheckFailed = 14,
-            ///<summary>操作失败</summary>
-            ActionFailed = 15
-        }
+			///<summary>参数错误</summary>
+			ParamErr = 13,
 
-        public readonly static HttpResult Success = new HttpResult(Stauts.Success, "ok");
-        public readonly static HttpResult Undefine = new HttpResult(Stauts.Undefine, "undefine command");
-        private readonly JsonSerializerOptions options = new JsonSerializerOptions
-        {
-            Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
-            //WriteIndented = true
-        };
-        public static HttpResult CreateOk(string retMsg = "")
-        {
-            return new HttpResult(Stauts.Success, retMsg);
-        }
+			///<summary>验证失败</summary>
+			CheckFailed = 14,
 
-        public static HttpResult CreateErrorParam(string retMsg = "")
-        {
-            return new HttpResult(Stauts.ParamErr, retMsg);
-        }
+			///<summary>操作失败</summary>
+			ActionFailed = 15
+		}
 
-        public static HttpResult CreateActionFailed(string retMsg = "")
-        {
-            return new HttpResult(Stauts.ActionFailed, retMsg);
-        }
+		public static readonly HttpResult Success = new HttpResult(Stauts.Success, "ok");
+		public static readonly HttpResult Undefine = new HttpResult(Stauts.UnDefine, "undefine command");
 
-        public string Code { get; set; }
-        public string Msg { get; set; }
-        public Dictionary<string, string> ExtraMap { get; set; }
-        public HttpResult(Stauts retCode = Stauts.Success, string retMsg = "ok")
-        {
-            Code = retCode.ToString();
-            Msg = retMsg;
-        }
+		private readonly JsonSerializerOptions options = new JsonSerializerOptions
+		{
+			Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
 
-        public string Get(string key)
-        {
-            if (ExtraMap == null)
-                return null;
-            ExtraMap.TryGetValue(key, out var res);
-            return res;
-        }
+			//WriteIndented = true
+		};
 
-        public void Set(string key, string value)
-        {
-            if (ExtraMap == null)
-                ExtraMap = new Dictionary<string, string>();
-            ExtraMap[key] = value;
-        }
+		public HttpResult(Stauts retCode = Stauts.Success, string retMsg = "ok")
+		{
+			Code = (int)retCode;
+			Msg = retMsg;
+		}
 
-        public override string ToString()
-        {
-            return JsonSerializer.Serialize(this, GetType(), options);
-        }
+		public int Code { get; set; }
+		public string Msg { get; set; }
+		public Dictionary<string, string> ExtraMap { get; set; }
 
-        public static implicit operator string(HttpResult value)
-        {
-            return value.ToString();
-        }
-    }
+		public static HttpResult CreateOk(string retMsg = "")
+		{
+			return new HttpResult(Stauts.Success, retMsg);
+		}
+
+		public static HttpResult CreateErrorParam(string retMsg = "")
+		{
+			return new HttpResult(Stauts.ParamErr, retMsg);
+		}
+
+		public static HttpResult CreateActionFailed(string retMsg = "")
+		{
+			return new HttpResult(Stauts.ActionFailed, retMsg);
+		}
+
+		public string Get(string key)
+		{
+			if (ExtraMap == null)
+				return null;
+			ExtraMap.TryGetValue(key, out var res);
+			return res;
+		}
+
+		public void Set(string key, string value)
+		{
+			if (ExtraMap == null)
+				ExtraMap = new Dictionary<string, string>();
+			ExtraMap[key] = value;
+		}
+
+		public override string ToString()
+		{
+			return JsonSerializer.Serialize(this, GetType(), options);
+		}
+
+		public static implicit operator string(HttpResult value)
+		{
+			return value.ToString();
+		}
+	}
 }
