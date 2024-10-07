5e386ec45d689e961fc845b3e0ffa230f08d4b41
OhJiang <14795402723@163.com>
2024-10-07 17:35:31 +0800

    feat: 新增钻石增加 & 钻石查询接口

---
+++ Geek.Server.Hotfix/Logic/Http/HttpAddDiamondHandler.cs
@@ -0,0 +1,33 @@
+﻿using Geek.Server.App.Logic.Account;
+using Geek.Server.Core.Net.Http;
+using Geek.Server.Core.Storage;
+
+namespace Server.Logic.Logic.Http
+{
+	[HttpMsgMapping("add_diamond")]
+	public class HttpAddDiamondHandler : BaseHttpHandler
+	{
+		public override async Task<string> Action(string ip, string url, Dictionary<string, string> paramMap)
+		{
+			if (!paramMap.TryGetValue("user_Id", out string userId) ||
+				!paramMap.TryGetValue("account_Id", out string accountId) ||
+				!paramMap.TryGetValue("diamond", out string diamond)) return "error";
+
+			if (userId.IsNullOrEmpty()) return HttpResult.CreateErrorParam();
+
+			if (!long.TryParse(accountId, out long accountIdLong) && accountIdLong <= 0) return HttpResult.CreateErrorParam();
+
+			if (!long.TryParse(diamond, out long diamondLong) && diamondLong <= 0) return HttpResult.CreateErrorParam();
+
+			var accountState = await GameDB.CreateQueryBuilder<AccountState>()
+			   .AddFilter("UserId", userId)
+			   .Load();
+
+			if (accountState == null) return HttpResult.CreateErrorParam();
+
+			accountState.Diamond = diamondLong;
+			await GameDB.UpdateField<AccountState, long>(accountIdLong, "Diamond", accountState.Diamond);
+			return HttpResult.CreateOk(accountState.Diamond.ToString());
+		}
+	}
+}
---
+++ Geek.Server.Hotfix/Logic/Http/HttpQueryDiamondHandler.cs
@@ -0,0 +1,27 @@
+﻿using Geek.Server.App.Logic.Account;
+using Geek.Server.Core.Net.Http;
+using Geek.Server.Core.Storage;
+
+namespace Server.Logic.Logic.Http
+{
+	[HttpMsgMapping("query_diamond")]
+	public class HttpQueryDiamondHandler : BaseHttpHandler
+	{
+		public override async Task<string> Action(string ip, string url, Dictionary<string, string> paramMap)
+		{
+			if (!paramMap.TryGetValue("user_Id", out string userId) ||
+				!paramMap.TryGetValue("account_Id", out string accountId)) return HttpResult.CreateErrorParam();
+
+			if (userId.IsNullOrEmpty()) return HttpResult.CreateErrorParam();
+
+			if (!long.TryParse(accountId, out long accountIdLong) && accountIdLong <= 0) return HttpResult.CreateErrorParam();
+
+			var accountState = await GameDB.CreateQueryBuilder<AccountState>()
+			   .AddFilter("UserId", userId)
+			   .Load();
+
+			if (accountState == null) return HttpResult.CreateErrorParam();
+			return HttpResult.CreateOk(accountState.Diamond.ToString());
+		}
+	}
+}
